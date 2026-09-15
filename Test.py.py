f61a6ff7cef1f9235b435f86c30d952e85091c27
Nathan20000000 <99082064@mydavinci.nl>
2026-09-15 11:03:51 +0200

    Fix factorial function and main check

--- Test.py.py
+++ Test.py.py
@@ -2,12 +2,17 @@ def faculteit(n):
     totaal = 1
     while n > 1:
         totaal *= n
-    return n
+        n -= 1
+    return totaal
 
-if __main__ == "__main__" :
+if __name__ == "__main__" :
+    print("-- Start Test --")
+    
     print(faculteit(0))
     print(faculteit(1))
     print(faculteit(2))
     print(faculteit(3))
     print(faculteit(4))
     print(faculteit(5))
+    print("-- Einde Test --")
+    
