30611a548bec22f514a258827b0f31b30e3d8b24
Nathan20000000 <99082064@mydavinci.nl>
2026-09-15 10:58:39 +0200

    Add test cases for faculteit function
    Added test cases for the faculteit function.

--- Test.py.py
+++ Test.py.py
@@ -3,3 +3,11 @@ def faculteit(n):
     while n > 1:
         totaal *= n
     return n
+
+if __main__ == "__main__" :
+    print(faculteit(0))
+    print(faculteit(1))
+    print(faculteit(2))
+    print(faculteit(3))
+    print(faculteit(4))
+    print(faculteit(5))
